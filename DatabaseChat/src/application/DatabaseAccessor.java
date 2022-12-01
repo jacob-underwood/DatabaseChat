4b2ba370ed7f9651dc062446848f516c975c0284
jacob-underwood <jacobu002@gmail.com>
2022-11-30 23:47:33 -0700

    Finished implementation of DatabaseAccessor.
    Added update and getKeys methods. Also got a starting point for Main
    that should likely be refactored and needs more work.

--- DatabaseChat/src/application/DatabaseAccessor.java
+++ DatabaseChat/src/application/DatabaseAccessor.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -8,18 +11,19 @@ import java.util.ArrayList;
  *
  */
 public class DatabaseAccessor {
-	
+
 	Connection c;
 	String table;
-	
+
 	/**
-	 * @param c Connection to database.
+	 * @param c     Connection to database.
 	 * @param table Name of the SQL table to be dealt with in the database.
 	 */
 	public DatabaseAccessor(Connection c, String table) {
-		
+		this.c = c;
+		this.table = table;
 	}
-	
+
 	/**
 	 * Adds an entry to the database for the specified table.
 	 * 
@@ -27,23 +31,169 @@ public class DatabaseAccessor {
 	 * @return true if successful
 	 */
 	public boolean add(ArrayList<Object> vals) {
-		return false;
+
+		/*
+		 * Orders for different tables: authentication: (ID, USERNAME, PASSWORD)
+		 * chat_room: (ROOM_ID, NAME, USERS) chat_history: (CHAT_ID, ROOM_ID, CHAT)
+		 */
+
+		// Collects all of the ArrayList entries into one String.
+		String valsStr = "";
+
+		// Strings in the ArrayList should have '' around them when put into the SQL
+		// command. Other data types should have nothing.
+		for (Object val : vals) {
+			if (val.getClass().equals(String.class)) {
+				valsStr += "'" + val.toString() + "', ";
+			} else {
+				valsStr += val.toString() + ", ";
+			}
+		}
+
+		// Fix the gate problem/remove the additional comma space at the end of valsStr.
+		valsStr = valsStr.substring(0, valsStr.length() - 2);
+
+		// Run PostreSQL command.
+		try {
+			Statement stmt = c.createStatement();
+			String sql = "INSERT INTO " + table + " VALUES(" + valsStr + ");";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+
+		return true;
 	}
-	
+
 	/**
 	 * @param id ID of element to delete.
 	 * @return true if successful.
 	 */
 	public boolean delete(int id) {
-		return false;
+
+		// Count of deleted rows.
+		int deleted = 0;
+
+		// Run PostreSQL command.
+		try {
+			Statement stmt = c.createStatement();
+			String sql = "DELETE FROM " + table + " WHERE id = " + id + ";";
+			deleted = stmt.executeUpdate(sql);
+			System.out.println(deleted);
+			stmt.close();
+			c.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+
+		return deleted > 0;
 	}
-	
+
 	/**
 	 * @param id ID to get values from.
-	 * @return true if successful.
+	 * @return Values to return, including id.
 	 */
 	public ArrayList<Object> get(int id) {
-		return null;
+
+		ArrayList<Object> vals = new ArrayList<>();
+
+		// Run PostreSQL command.
+		try {
+			Statement stmt = c.createStatement();
+			String sql = "SELECT * FROM " + table + " WHERE id = " + id + ";";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+
+			while (rs.next()) {
+				for (int i = 1; i <= columnsNumber; i++) {
+					Object val = rs.getObject(i);
+					vals.add(val);
+				}
+			}
+
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+
+		return vals;
 	}
-	
+
+	/**
+	 * Gets all of the unique IDs in the table.
+	 * 
+	 * @return Keys in an ArrayList of Integers.
+	 */
+	public ArrayList<Integer> getKeys() {
+
+		ArrayList<Integer> keys = new ArrayList<Integer>();
+
+		// Run PostreSQL command.
+		try {
+			Statement stmt = c.createStatement();
+			String sql = "SELECT id FROM " + table + ";";
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				int key = rs.getInt(1);
+				keys.add(key);
+			}
+
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+
+		return keys;
+	}
+
+	/**
+	 * Updates a value in the table at the specified id in the given column.
+	 * 
+	 * @param id ID of row to update (primary key).
+	 * @param column Name of column to update.
+	 * @param val New value to set column at id to.
+	 * @return true if successful.
+	 */
+	public boolean update(int id, String column, Object val) {
+
+		// String version of val.
+		String valStr = "";
+
+		// Strings in the ArrayList should have '' around them when put into the SQL
+		// command. Other data types should have nothing.
+		if (val.getClass().equals(String.class)) {
+			valStr += "'" + val.toString() + "'";
+		} else {
+			valStr += val.toString();
+		}
+
+		// Run PostreSQL command.
+		try {
+			Statement stmt = c.createStatement();
+			String sql = "UPDATE " + table + " SET " + column + " = " + valStr + " WHERE id = " + id + ";";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+
+		return true;
+	}
+
 }
--- DatabaseChat/src/application/Main.java
+++ DatabaseChat/src/application/Main.java
@@ -1,18 +1,143 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// Create Connection to database.
 		
+		String database = "databasechat";
+
+		Connection c = null;
+		Statement stmt = null;
+
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + database, "postgres",
+					"the2@d7idoNedwCYoE3k");
+			
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+			
+			DatabaseMetaData md = c.getMetaData();
+			ResultSet rs = md.getTables(null, null, "%", null);
+			int numTables = 0;
+//			if (rs != null) {
+//				rs.last();
+//				numTables = rs.getRow();
+//			}
+			while(rs.next()) {
+				if (rs.getString(2).equals("public") && !rs.getString(3).contains("pkey")) {
+					numTables++;
+				}
+			}
+			System.out.println(numTables);
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		
+//		try {
+//			stmt = c.createStatement();
+//			String sql = "CREATE TABLE authentication " +
+//					"(id INT PRIMARY KEY NOT NULL," +
+//					" username TEXT NOT NULL," +
+//					" password TEXT NOT NULL);";
+//			stmt.executeUpdate(sql);
+//			stmt.close();
+//			c.close();
+//			System.out.println("Table was created");
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+//			System.exit(0);
+//		}
+		
+//		try {
+//			stmt = c.createStatement();
+//			String sql = "CREATE TABLE chat_room " +
+//					"(room_id INT PRIMARY KEY NOT NULL," +
+//					" name TEXT NOT NULL," + 
+//					" users TEXT[] NOT NULL);";
+//			stmt.executeUpdate(sql);
+//			stmt.close();
+//			c.commit();
+//			c.close();
+//			System.out.println("Table was created");
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+//			System.exit(0);
+//		}
+		
+//		try {
+//			stmt = c.createStatement();
+//			String sql = "CREATE TABLE chat_history " +
+//					"(chat_id INT PRIMARY KEY NOT NULL," +
+//					" room_id INT NOT NULL," + 
+//					" chat TEXT NOT NULL);";
+//			stmt.executeUpdate(sql);
+//			stmt.close();
+//			c.commit();
+//			c.close();
+//			System.out.println("Table was created");
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			System.err.print(e.getClass().getName() + ": " + e.getMessage());
+//			System.exit(0);
+//		}
+
+		DatabaseAccessor authenticationAccessor = new DatabaseAccessor(c, "authentication");
+		DatabaseAccessor chatAccessor = new DatabaseAccessor(c, "chat_room");
+		DatabaseAccessor historyAccessor = new DatabaseAccessor(c, "chat_history");
+		
+		// EXAMPLE OF USAGE.
+		
+//		ArrayList<Object> test = new ArrayList<>();
+//		test.add(30);
+//		test.add("jacob1");
+//		test.add("password123");
+//		
+//		authenticationAccessor.add(test);
+//		
+//		authenticationAccessor.delete(30);
+//		
+//		authenticationAccessor.update(30, "username", "jacob2");
+//		
+//		ArrayList<Object> returned = new ArrayList<>();
+//		
+//		returned = authenticationAccessor.get(30);
+//		
+//		System.out.println(returned);
+//		
+//		System.out.println(authenticationAccessor.getKeys());
+
+		// OUTDATED:
+		
 		// while loop -> Start Authentication screen.
-		
+
 		// User object to be used throughout program.
-		
+
 		// while loop -> MainView screen.
+
+		// while loop -> ChatRoom screens.
+
 		
-			// while loop -> ChatRoom screens.
-		
+
+		try {
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 }
