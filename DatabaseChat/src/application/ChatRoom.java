c8ff52b93b2fe0e4531e0957dfd2ea711153a84d
Diya Hamada <dhamada@asu.edu>
2022-12-01 23:05:19 -0700

    Started implementation

--- DatabaseChat/src/application/ChatRoom.java
+++ DatabaseChat/src/application/ChatRoom.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.sql.DriverManager;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 /**
  * User view upon entering a chat room.
  * Allows user to send a chat.
@@ -8,13 +12,47 @@ package application;
  */
 public class ChatRoom {
 	
+	User user;
+	String roomName;
+	int roomId;
+	ArrayList<User> users = new ArrayList<>();
+	DatabaseAccessor roomInfoAccessor;
+	DatabaseAccessor historyAccessor;
+	Scanner input;
+	
+	boolean leaving = false;
+	
+	public ChatRoom(User user, String roomName, int roomId, DatabaseAccessor roomInfoAccessor, DatabaseAccessor historyAccessor, Scanner input) {
+
+		user = this.user;
+		roomName = this.roomName;
+		roomId = this.roomId;
+		roomInfoAccessor = this.roomInfoAccessor;
+		historyAccessor = this.historyAccessor;
+		input = this.input;
+		
+	}
+		
+	
 	/**
 	 * Takes a command marked by a "/" and executes appropriate method.
 	 * 
 	 * @param cmd The command inputed.
 	 */
-	public void parseCommand(String cmd) {
+	private void parseCommand(String command) {
 		
+		String cmd = command.substring(1);
+		if(cmd.compareTo("list") == 0)
+			list();
+		else if(cmd.compareTo("leave") == 0)
+			leave();
+		else if(cmd.compareTo("history") == 0)
+			history();
+		else if(cmd.compareTo("help") == 0)
+			help();
+		else
+			System.out.println("Invalid command! Type /help to see available commands.");
+			
 	}
 	
 	/**
@@ -22,20 +60,48 @@ public class ChatRoom {
 	 */
 	private void list() {
 		
+		System.out.println("Users in chatroom " + roomName + " : ");
+		
+		ArrayList<Integer> userList = 
+		
+		for(int i = 0; i < userList.size(); i++)
+		{
+			System.out.println(i+1 + ". " + userList.get(i));
+		}
+		
 	}
 
 	/**
-	 * User exits chat room.
+	 * User exits chat room into MainView.
 	 */
 	private void leave() {
-	
+		
+		System.out.println("Leaving chatroom.");
+		
+		leaving = true;
+		roomInfoAccessor.delete(user.getId());
+		
 	}
 
 	/**
 	 * Prints all past messages for current chat room.
 	 */
 	private void history() {
-	
+		
+		System.out.println("Displaying history for chatroom" + roomName);
+		
+		if(historyAccessor.getKeys().size() == 0) {
+			
+			System.out.println("No chat history!");
+			
+		}
+		
+		for(int i = 0; i < historyAccessor.getKeys().size(); i++) {
+			
+			historyAccessor.get(i)
+			
+		}
+		
 	}
 
 	/**
@@ -43,6 +109,11 @@ public class ChatRoom {
 	 */
 	private void help() {
 		
+		System.out.println("Available commands :");
+		System.out.println("/list - Prints the list of users currently in this chat room.");
+		System.out.println("/leave - Exit the chat room.");
+		System.out.println("/history - Print all past messages for this room.");
+		System.out.println("/help for help.");
 	}
 	
 	/**
@@ -52,12 +123,25 @@ public class ChatRoom {
 	 */
 	public void sendChat(String msg) {
 		
+		System.out.print(msg);
+		
+		if(msg.substring(0,1).equals("/"))
+			parseCommand(msg);
+		
+		update(msg);
 	}
 	
 	/**
 	 * Updates chat history view for other user activity.
 	 */
-	public void update() {
+	public void update(String msg) {
+		
+		ArrayList<Integer> entry = new ArrayList<>();
+		int chatId = historyAccessor.getKeys().size();
+		
+		entry.add(chatId, roomId, msg);
+		
+		historyAccessor.add(entry);
 		
 	}
 }
--- DatabaseChat/src/application/MainView.java
+++ DatabaseChat/src/application/MainView.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 /**
  * Primary view of application upon authenticating.
  * Allows creation and joining of rooms.
@@ -7,23 +9,85 @@ package application;
  * Allows logout.
  */
 public class MainView {
-
+	
+	User user;
+	DatabaseAccessor roomInfoAccessor;
+	DatabaseAccessor historyAccessor;
+	Scanner input = new Scanner(System.in);
+	
+	boolean activeRoom = false;
+	
 	/**
 	 * Creates new chatroom.
 	 * 
-	 * @param rName The name of the new room.
 	 */
-	public void createRoom(String rName) {
-
+	public void createRoom() {
+		
+		String name = "";
+		boolean valid = false;
+		
+		while(!valid) {
+			System.out.println("Enter a name for your chat room : ");
+			name = input.nextLine();
+			if(name.equals("/exit")) //exit to main view
+				valid = true;
+			else if() //name is taken
+				System.out.println("A room already exists with that name. Please enter another: ");
+			else { 
+				//valid room name! create room and enter
+				valid = true;
+				Chatroom room = new Chatroom(user, name, roomInfoAccessor.getKeys().size(), roomInfoAccessor, historyAccessor, input);
+				String msg = "";
+				while(!room.leaving()) {
+					
+					System.out.println("Room " + name + " created!");
+					System.out.println(user + ": ");
+					msg = input.nextLine();
+					room.sendChat(msg);
+					
+				}
+			}
+		}
+		
+		System.out.println("Returning to Main View.");
+		
 	}
 
 	/**
-	 * Adds user to existing chat room. Displays error if room already exists.
+	 * Adds user to existing chat room. Displays error if room does not exist.
 	 * 
 	 * @param rName The name of the room to be joined.
 	 */
 	public void joinRoom(String rName) {
-
+		
+		String name = "";
+		boolean valid = false;
+		
+		while(!valid) {
+			System.out.println("Enter the name of the room you want to join : ");
+			name = input.nextLine();
+			if(name.equals("/exit")) //exit to main view
+				valid = true;
+			else if() //name is taken
+				System.out.println("A room already exists with that name. Please enter another: ");
+			else { 
+				//valid room name! join room
+				valid = true;
+				Chatroom room = new Chatroom(user, name, roomInfoAccessor.getKeys().size(), roomInfoAccessor, historyAccessor, input);
+				String msg = "";
+				while(!room.leaving()) {
+					
+					System.out.println("Room " + name + " created!");
+					System.out.println(user + ": ");
+					msg = input.nextLine();
+					room.sendChat(msg);
+					
+				}
+			}
+		}
+		
+		System.out.println("Returning to Main View.");
+		
 	}
 
 	/**
@@ -32,7 +96,31 @@ public class MainView {
 	 * @param name The new user name.
 	 */
 	public void updateUser(String name) {
-
+		
+		boolean valid = false;
+		String name = "";
+		
+		while(!valid) {
+			
+			System.out.println("Enter a new username : ");
+			name = input.nextLine();
+			
+			if(name.equals("/exit")) //exit to main view
+				valid = true;
+			else if() //name is taken
+				System.out.println("A user already exists with that name. Please enter another: ");
+			else { 
+				//valid user name! update name
+				valid = true;
+				
+					
+				}
+			}
+			
+		}
+		
+		System.out.println("Returning to Main View.");
+		
 	}
 
 	/**
@@ -41,14 +129,40 @@ public class MainView {
 	 * @param newPass The new password.
 	 */
 	public void updatePass(String newPass) {
-
+		
+		boolean valid = false;
+		String name = "";
+		
+		while(!valid) {
+			
+			System.out.println("Enter a new username : ");
+			name = input.nextLine();
+			
+			if(name.equals("/exit")) //exit to main view
+				valid = true;
+			else if() //name is taken
+				System.out.println("A user already exists with that name. Please enter another: ");
+			else { 
+				//valid user name! update name
+				valid = true;
+				
+					
+				}
+			}
+			
+		}
+		
+		System.out.println("Returning to Main View.");
+		
 	}
 
 	/**
 	 * Logs the current user out. Return to authentication.
 	 */
 	public void logout() {
-
+		
+		
+		
 	}
 
 }
